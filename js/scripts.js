bc209858005a03a54d63eee9e63cc265d6930466
Le <mle7@lab-che204-m22.eastern.ewu.edu>
2019-02-13 10:46:15 -0800

    Added 3 codepens

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,9 @@
 console.log('OK');
 
 // Your Awesome Scripts
+$('img').show();
+$('iframe').show();
+$('iframe').ready(function(){
+    $('iframe').fitVids();
+    });
+console.log('I ❤ JavaScript');
